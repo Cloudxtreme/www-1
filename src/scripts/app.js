a6e3394026e7860e24b3af32f2a7d82071f9424d
Jason Maurer <j@maur.co>
2015-12-25 14:53:48 -0700

    coming soon page release

---
+++ src/scripts/app.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+
+  // $.stellar({
+  //   // hideDistantElements: false,
+  //   horizontalScrolling: false
+  // })
+  // skrollr.init()
+
+  /**
+   * for playing vimeo video
+   */
+
+  // let video = document.getElementById('video')
+  //
+  // $('#play-video').on('click', (e) => {
+  //   let self = $(e.target)
+  //   self.fadeOut()
+  //   self.next().fadeIn()
+  //   $f(video).api('play')
+  // })
+
+})
