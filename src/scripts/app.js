e1bb06af7f74fae9e57de6992eafdf5b555a6333
Jason Maurer <j@maur.co>
2016-03-16 17:48:46 -0600

    demo video

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,24 +1,43 @@
 import $ from 'jquery'
 
+let player, isReady
+global.onYouTubePlayerAPIReady = () => {
+  player = new YT.Player('video', {
+    events: {
+      'onReady': () => {
+        isReady = true
+      }
+    }
+  })
+}
+
 $(document).ready(() => {
 
   // $.stellar({
   //   // hideDistantElements: false,
   //   horizontalScrolling: false
   // })
-  // skrollr.init()
+
+  skrollr.init()
 
   /**
    * for playing vimeo video
    */
 
-  // let video = document.getElementById('video')
-  //
-  // $('#play-video').on('click', (e) => {
-  //   let self = $(e.target)
-  //   self.fadeOut()
-  //   self.next().fadeIn()
-  //   $f(video).api('play')
-  // })
+  $('.video').on('click', (e) => {
+    let video = $('#play-video')
+
+    if (isReady) {
+      video.fadeOut()
+      video.next().fadeIn()
+      player.playVideo()
+    }
+  })
 
 })
+
+
+var tag = document.createElement('script')
+tag.src = "//www.youtube.com/player_api"
+var firstScriptTag = document.getElementsByTagName('script')[0]
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
